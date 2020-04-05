b6f72b8d0590063817da604e8c64a034f736fcc4
Jensen David <feudavid92@gmail.com>
2020-04-05 12:13:06 -0700

    deleted commented code

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import glob
 import os
 
 
-# print("Building Static Sites")
 
 
 pages = []
@@ -27,7 +26,7 @@ def generate_list():
 
 
 
-# loop
+
 def generate_pages():
 	generate_list()
 
@@ -64,53 +63,3 @@ def new_page():
 
 
 
-# for page in pages:
-	# 	file_name = page["file_name"]
-	# 	output = page["output"]
-	# 	title = page["title"]
-	# 	content = open(file_name).read()
-	# 	full_page = apply_template(content, title)
-	# 	final_output = produce_output(output, full_page)
-		
-
-
-
-# #templating
-# def apply_template(content, title):
-# 	template = open("templates/base.html").read()
-# 	template_with_content = template.replace("{{content}}", content)
-# 	full_page = template_with_content.replace("{{title}}", title)
-# 	return full_page
-
-
-# #produce output
-# def produce_output(output, full_page):
-# 	open(output, 'w+').write(full_page)
-
-# if __name__ == "__main__":
-# 	main()
-
-
-
-#previous code for reference.
-
-# print('building static site')
-# 	top_html = open('templates/top.html').read()
-# 	bottom_html = open('templates/bottom.html').read()
-
-# 	#index
-# 	middle_html = open('content/index.html').read()
-# 	combined_html = top_html + middle_html + bottom_html
-# 	open('docs/index.html', 'w+').write(combined_html)
-
-# 	#projects
-# 	print('building static site')
-# 	middle_html = open('content/projects.html').read()
-# 	combined_html = top_html + middle_html + bottom_html
-# 	open('docs/projects.html', 'w+').write(combined_html)
-
-# 	#contact
-# 	print('building static site')
-# 	middle_html = open('content/contact.html').read()
-# 	combined_html = top_html + middle_html + bottom_html
-# 	open('docs/contact.html', 'w+').write(combined_html)
